6e19f3176c627d8bc2e5d2c66609bbde91040ad0
dhruvmishra482 <dhruvmishra3829@gmail.com>
2025-08-20 23:11:07 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>App 
+
+     <h1 className="text-5xl font-bold underline   ">
+      Hello world!
+    </h1>
+    <h2 className='text-red-300'>hello</h2>
+
+    </div>
+    
+  )
+}
+
+export default App
